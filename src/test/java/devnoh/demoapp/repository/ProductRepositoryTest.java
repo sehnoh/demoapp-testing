2bf47ea412079195e36d471eb001b83279519373
Sehwan Noh <sehwan.noh@autogravity.com>
2018-02-06 09:44:51 -0800

    Update unit test

--- src/test/java/devnoh/demoapp/repository/ProductRepositoryTest.java
+++ src/test/java/devnoh/demoapp/repository/ProductRepositoryTest.java
@@ -10,9 +10,12 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,9 +28,13 @@ public class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
+    private Product product1;
+
+    private Product product2;
+
     @Before
     public void setUp() throws Exception {
-        Product product1 = new Product();
+        product1 = new Product();
         product1.setCode("P001");
         product1.setName("iPhone 7");
         product1.setDescription("This is an Apple phone.");
@@ -35,7 +42,7 @@ public class ProductRepositoryTest {
 
         entityManager.persist(product1);
 
-        Product product2 = new Product();
+        product2 = new Product();
         product2.setCode("P002");
         product2.setName("Pixel 2");
         product2.setDescription("This is a Google phone.");
@@ -48,10 +55,7 @@ public class ProductRepositoryTest {
     public void findByCode() {
         Product product = productRepository.findByCode("P001");
         assertNotNull(product);
-        assertEquals("P001", product.getCode());
-        assertEquals("iPhone 7", product.getName());
-        assertEquals("This is an Apple phone.", product.getDescription());
-        assertEquals(true, product.isActive());
+        assertEquals(product1, product);
     }
 
     @Test
@@ -62,24 +66,47 @@ public class ProductRepositoryTest {
 
     @Test
     public void findByName() {
+        List<Product> products = productRepository.findByName("Pixel 2");
+        assertEquals(1, products.size());
+        assertEquals(product2, products.get(0));
     }
 
     @Test
     public void findByActiveIsTrue() {
+        List<Product> products = productRepository.findByActiveIsTrue();
+        assertEquals(1, products.size());
+        assertEquals(product1, products.get(0));
     }
 
     @Test
     public void testCRUD() {
         Product product = new Product();
-        product.setCode("P001");
-        product.setName("iPhone 7");
+        product.setCode("P003");
+        product.setName("iPhone 8");
         product.setDescription("This is an Apple phone.");
         product.setActive(true);
-
         productRepository.save(product);
 
         Product found = productRepository.getOne(product.getId());
         assertNotNull(found);
         assertEquals(product, found);
+
+        product.setName("iPhone X");
+        productRepository.save(product);
+
+        found = productRepository.getOne(product.getId());
+        assertEquals("iPhone X", found.getName());
+
+        productRepository.delete(product.getId());
+
+        found = productRepository.findOne(product.getId());
+        assertNull(found);
+
+        try {
+            found = productRepository.getOne(product.getId());
+            fail();
+        } catch (Exception e) {
+            // pass
+        }
     }
 }
